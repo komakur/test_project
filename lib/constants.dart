71a6fd5813cf9b6a9e826496a81748c3f0eebdc5
kostvick <my.mail.kostya@gmail.com>
2022-08-18 01:05:03 +0300

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/// Class which contains constants
+class Constants {
+  /// Default Style of Main Text in the Centre of Main Screen
+  static const kDefaultMainTextStyle = TextStyle(
+    fontFamily: 'Silkscreen',
+    fontWeight: FontWeight.bold,
+    fontSize: 50.0,
+  );
+
+  /// Default Style for Bottom Hint Text
+  static const kDefaultBottomHintTextStyle = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 15.0,
+    color: Colors.blueGrey,
+  );
+
+  /// Default Padding for Bottom Hint Text
+  static const kDefaultBottomHintTextPadding = 30.0;
+}
