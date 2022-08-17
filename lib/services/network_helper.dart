6ee40eb3baa2446664b83129ebc1ac4837fa8102
kostvick <my.mail.kostya@gmail.com>
2022-08-18 01:07:49 +0300

    manage data from api

--- lib/services/network_helper.dart
+++ lib/services/network_helper.dart
@@ -1,4 +1,25 @@
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-class Networking {}
+/// Manages data from APIs
+class NetworkHelper {
+  /// URL to make a request to API
+  final String url;
+
+  ///  Constructor which takes URL to make a request
+  NetworkHelper({required this.url});
+
+  /// Gets data from API and returns Json object
+  Future<String> getData() async {
+    final http.Response response = await http.get(Uri.parse(url));
+
+    const int successStatusCode = 200;
+
+    if (response.statusCode == successStatusCode) {
+      final String data = response.body;
+
+      return data;
+    } else {
+      return 'Wrong request';
+    }
+  }
+}
